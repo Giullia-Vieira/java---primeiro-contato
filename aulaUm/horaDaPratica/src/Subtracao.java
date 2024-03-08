5f5a97995562220475fa04e9103aa43fcbfc1b8a
Giullia Isabel Vieria da Cunha <131213404+Giullia-Vieira@users.noreply.github.com>
2024-03-07 23:36:34 -0300

    primeira aula
    primeira aula iniciei com o famoso "Hello wordl!" e entendendo os operadores

---
+++ aulaUm/horaDaPratica/src/Subtracao.java
@@ -0,0 +1,11 @@
+package horaDaPratica.src;
+
+public class Subtracao {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int subtracao = a - b;
+
+        System.out.println("a subtração é: " + subtracao);
+    }
+}
---
+++ aulaUm/horaDaPratica/src/somar.java
@@ -0,0 +1,13 @@
+package horaDaPratica.src;
+
+public class somar {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int soma = a + b;
+
+        System.out.println("a soma é: " + soma);
+
+    }
+}
